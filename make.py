f1a09035b2a27783c63b3474c5dfcf2cef414a16
unknown <jean.huaylla@utec.edu.pe>
2025-06-30 19:11:22 -0500

    script

--- make.py
+++ make.py
@@ -1,15 +1,21 @@
 import subprocess
 import os
 from optimizacion import optimize_code 
+import sys
 
 # Carpeta de entrada
 input_dir = "inputs"
 
 # Archivos fuente
-source_files = [
-    "main.cpp", "parser.cpp", "scanner.cpp", "token.cpp",
-    "visitor.cpp", "exp.cpp"
-]
+#source_files = [
+#    "main.cpp", "parser.cpp", "scanner.cpp", "token.cpp",
+#    "visitor.cpp", "exp.cpp"
+#]
+source_files=['*.cpp']
+
+if not source_files:
+    print("No se encontraron archivos .cpp")
+    sys.exit(1)
 
 # Compilar
 print("Compilando...")
@@ -33,5 +39,5 @@ for i in range(1, 15):
     input_file_optimized= os.path.join(input_dir, f"input{i}_optimized.txt")
     optimize_code(input_file,input_file_optimized)
 
-    print(f"\nEjecutando con {input_file_optimized}")
-    subprocess.run(["./a.exe", input_file_optimized])
+    print(f"\nEjecutando con {input_file}")
+    subprocess.run(["./a.exe", input_file])
